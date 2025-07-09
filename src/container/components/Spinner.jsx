b22499b36a1bbafaf25ff2e47515f8bd04baa25a
williamchidi <williamchidi1@gmail.com>
2025-07-09 14:29:50 +0100

    completed order page

---
+++ src/container/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function Spinner() {
+  return (
+    <div className="w-[4rem] h-[4rem] border-4 border-gray-800 border-t-stone-100 border-t-4 rounded-full animate-spin"></div>
+  );
+}
+export default Spinner;
--- src/container/pages/transportAndLogistics/OrderPage.jsx
+++ src/container/pages/transportAndLogistics/OrderPage.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import Action from "../../components/Action";
 import Button from "../../components/Button";
 import HrProfileAndNotification from "../../components/HrProfileAndNotification";
 import PageNavigation from "../../components/PageNAvigation";
 import SearchBar from "../../components/SearchBar";
+import Spinner from "../../components/Spinner";
 
 const orders = [
   {
@@ -118,6 +120,17 @@ const orders = [
 ];
 
 function OrderPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [activeTab, setActiveTab] = useState("schedule");
+
+  function handleClick(x) {
+    setActiveTab(x);
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }
+
   return (
     <section className="flex-1 h-screen flex flex-col overflow-y-auto ">
       <header className="flex justify-between items-center py-4 px-4">
@@ -130,82 +143,107 @@ function OrderPage() {
           <HrProfileAndNotification />
         </div>
       </header>
-      <main className="px-4 py-4 border border-gray-600 rounded-md">
+      <main className="relative px-4 py-4 border border-gray-600 rounded-md">
         <div className="flex justify-end items-center gap-4 pb-4">
-          <Button bg="#6B7280" width="7rem">
+          <Button
+            bg={activeTab === "schedule" ? "#7152F3" : "#6B7280"}
+            width="7rem"
+            click={() => handleClick("schedule")}
+          >
             Sheduled Order
           </Button>
-          <Button bg="#7152F3" width="5rem">
+
+          <Button
+            bg={activeTab === "live" ? "#7152F3" : "#6B7280"}
+            width="5rem"
+            click={() => handleClick("live")}
+          >
             Live Order
           </Button>
         </div>
-        <div>
-          <header className="flex justify-between items-center bg-[#4D6A3F] text-[#f5f5f4] px-4 py-2 text-xs rounded-md">
-            <span className="flex-[1.5]">Service</span>
-            <span className="flex-1">Order ID</span>
-            <span className="flex-1">Date</span>
-            <span className="flex-1">PickUp Time</span>
-            <span className="flex-1">Driver</span>
-            <span className="flex-1">Status</span>
-            <span className="flex-1">End Time</span>
-            <span className="flex-1">Amount</span>
-            <span className="flex-1">Action</span>
-          </header>
+
+        <div
+          className={`transition-all duration-300 ${
+            isLoading ? "blur-sm pointer-events-none" : ""
+          }`}
+        >
           <div>
-            {orders.map((order, i) => (
-              <div
-                key={i}
-                className="flex justify-between items-center text-[.6rem] py-2 px-2 hover:bg-gray-800 hover:cursor-pointer"
-              >
-                <span className="flex-[1.5]">{order.service}</span>
-                <span className="flex-1">{order.orderId}</span>
-                <span className="flex-1">{order.date}</span>
-                <span className="flex-1">{order.pickUpTime}</span>
-                <span className="flex-1">{order.driver}</span>
-                <span className="flex-[1] flex justify-start items-center gap-1">
-                  <span
-                    className={`inline-flex w-[.3rem] h-[.3rem] rounded-full ${
-                      order.status === "Canceled"
-                        ? "bg-[#EF0606]"
-                        : order.status === "Pending"
-                        ? "bg-[#F29A2E]"
-                        : "bg-[#4CE13F]"
-                    }`}
-                  ></span>
-                  {order.status}
-                </span>
-                <span className="flex-1">{order.endTime}</span>
-                <span className="flex-1">${order.amount}.00</span>
-                <span className="flex-1">
-                  <Action />
-                  {/* <span className="inline-flex w-[.2rem] h-[.2rem] bg-stone-50 rounded-full"></span>
+            <header className="flex justify-between items-center bg-[#4D6A3F] text-[#f5f5f4] px-4 py-2 text-xs rounded-md">
+              <span className="flex-[1.5]">Service</span>
+              <span className="flex-1">Order ID</span>
+              <span className="flex-1">Date</span>
+              <span className="flex-1">PickUp Time</span>
+              <span className="flex-1">Driver</span>
+              <span className="flex-1">Status</span>
+              <span className="flex-1">End Time</span>
+              <span className="flex-1">Amount</span>
+              <span className="flex-1">Action</span>
+            </header>
+
+            <div>
+              {orders.map((order, i) => (
+                <div
+                  key={i}
+                  className="flex justify-between items-center text-[.6rem] py-2 px-2 hover:bg-gray-800 hover:cursor-pointer"
+                >
+                  <span className="flex-[1.5]">{order.service}</span>
+                  <span className="flex-1">{order.orderId}</span>
+                  <span className="flex-1">{order.date}</span>
+                  <span className="flex-1">{order.pickUpTime}</span>
+                  <span className="flex-1">{order.driver}</span>
+                  <span className="flex-[1] flex justify-start items-center gap-1">
+                    <span
+                      className={`inline-flex w-[.3rem] h-[.3rem] rounded-full ${
+                        order.status === "Canceled"
+                          ? "bg-[#EF0606]"
+                          : order.status === "Pending"
+                          ? "bg-[#F29A2E]"
+                          : "bg-[#4CE13F]"
+                      }`}
+                    ></span>
+                    {order.status}
+                  </span>
+                  <span className="flex-1">{order.endTime}</span>
+                  <span className="flex-1">${order.amount}.00</span>
+                  <span className="flex-1">
+                    <Action />
+                    {/* <span className="inline-flex w-[.2rem] h-[.2rem] bg-stone-50 rounded-full"></span>
                   <span className="inline-flex w-[.2rem] h-[.2rem] bg-stone-50 rounded-full"></span>
                   <span className="inline-flex w-[.2rem] h-[.2rem] bg-stone-50 rounded-full"></span> */}
-                </span>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex justify-between items-center px-4 pt-14">
-          <div className="space-x-3">
-            <span className="text-xs">Showing</span>
-            <select className=" bg-transparent text-stone-700 border border-gray-800 rounded-md text-xs focus:outline-none py-1 px-1">
-              {Array.from({ length: 10 }, (_, i) => (
-                <option className="text-xs">{i + 1}</option>
+                  </span>
+                </div>
               ))}
-            </select>
+            </div>
           </div>
-          <p className="text-xs">Showing 1 of 10 out of 60 records</p>
 
-          <PageNavigation
-            num={4}
-            color="#f5f5f4"
-            bg="transparent"
-            activeColor="#7152F3"
-            activeBg="transparent"
-            borderColor="#7152F3"
-          />
+          <div className="flex justify-between items-center px-4 pt-14">
+            <div className="space-x-3">
+              <span className="text-xs">Showing</span>
+              <select className=" bg-transparent text-stone-700 border border-gray-800 rounded-md text-xs focus:outline-none py-1 px-1">
+                {Array.from({ length: 10 }, (_, i) => (
+                  <option className="text-xs" key={i}>
+                    {i + 1}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <p className="text-xs">Showing 1 of 10 out of 60 records</p>
+
+            <PageNavigation
+              num={4}
+              color="#f5f5f4"
+              bg="transparent"
+              activeColor="#7152F3"
+              activeBg="transparent"
+              borderColor="#7152F3"
+            />
+          </div>
         </div>
+        {isLoading && (
+          <div className="absolute inset-0 z-10 flex justify-center items-center bg-black bg-opacity-30">
+            <Spinner />
+          </div>
+        )}
       </main>
     </section>
   );
